52187052e34b5acdec06a98dace479b594e48cba
Gabriel Nalbandian <gabriel.nalbandian94@gmail.com>
2020-12-01 18:04:23 +0200

    added movies API

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ TELEGRAM_TOKEN = '1366712014:AAGAyoQ7vX8IJ1vPd2p0LmD4LcqYTVv-Hr8'
 
 WEATHER_TOKEN = '6bc658857c1f4462b145b783ff7bd21e'
 
+MOVIES_TOKEN = '7b366656dc25bdc6b87250d0fd1ac0ae'
+
 url = "https://7910c78c1e58.ngrok.io/message"
 TELEGRAM_INIT_URL = 'https://api.telegram.org/bot{}/setWebhook?url={}'.format(TELEGRAM_TOKEN, url)
--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def message_handler():
 
     #print(request.get_json())    
     #bot.send_message(chat_id, "Hi, What's your location? Preferably the name of the city and country. Thank you")
-    """
+    """   -----   weather
     txt = request.get_json()['message'].get("text")
 
     weather = "https://api.weatherbit.io/v2.0/current?city={}&key={}"
@@ -22,10 +22,21 @@ def message_handler():
     bot.send_message(chat_id, "The weather in your city is currently " + str(temp.json()["data"][0]["temp"]) + " Degrees")
     """
 
+    """ ----- jokes
     joke = requests.get("https://official-joke-api.appspot.com/jokes/general/random")    
     bot.send_message(chat_id, joke.json()[0].get("setup"))
 
     bot.send_message(chat_id, joke.json()[0].get("punchline"))
+    """
+
+    
+    movies = "https://api.themoviedb.org/3/discover/movie?api_key={}&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page=1&with_genres=35&primary_release_date.gte=2015-01-01&primary_release_date.lte=2015-12-31&with_original_language=en"
+    comedy = requests.get(movies.format(MOVIES_TOKEN))
+    movies_list = []
+    for i in range(10):    
+        movies_list.append(comedy.json()["results"][i]["title"])
+
+    bot.send_message(chat_id, movies_list)            
 
     return Response("Server is up and running smoothly")
 
