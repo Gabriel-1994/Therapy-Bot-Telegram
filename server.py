53331c565d312c218b67edcd10a8c055b54c9b60
Apkar <apkar96@gmail.com>
2020-12-01 22:48:06 +0200

    "merge conflict resolved"

--- server.py
+++ server.py
@@ -2,27 +2,24 @@ from flask import Flask, Response, request
 from config import *
 import requests
 from Bot.Bot import *
-from analyzer import analyze_text
-from recipe import get_recipes
 import json
+
 
 
 app = Flask(__name__)
 
-
 @app.route('/message', methods=["POST"])
 def message_handler():
-    
-
     args = request.get_json()['message']['text'].split()
     chat_id = request.get_json()['message']['chat']['id']
-    print(chat_id)
+
     bot.action(args,chat_id,request.get_json())
+    
     return Response("Server is up and running smoothly")
 
 
 
 if __name__ == '__main__':
-    bot = get_bot()
     requests.get(TELEGRAM_INIT_URL)
-    app.run(port=5003)
+    bot = get_bot()
+    app.run(port=5002)
