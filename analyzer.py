93e3a91fd5a1e481fa274a6279104d24c73cbb1a
mohamedsl22 <moodi.salman@gmail.com>
2020-12-01 16:37:08 +0200

    c3

--- analyzer.py
+++ analyzer.py
@@ -11,6 +11,17 @@ def analyze_text(text):
     return pos, neg
 
 
+def compare_mood(prev, now):
+    positive = 0
+    negative = 1
+    if prev[positive] < now[positive] and prev[negative] > now[negative]:
+        return "better"
+    elif prev[positive] < now[positive] and prev[negative] > now[negative]:
+        return "better positive"
+    else:
+        return "worse"
+
+
 def analyze_text_w(text):
     paralleldots.set_api_key(paralleldots_TOKEN)
     paralleldots.get_api_key()
